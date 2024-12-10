0ec519583863038c31bf50b40c3fc19da9b0af28
mxqt <maxquinntapara@gmail.com>
2024-12-11 10:58:49 +1300

    round 1

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
-document.addEventListener("scroll", () => {
-    const scrollPosition = window.scrollY;
-    const images = document.querySelector(".images");
-    const text = document.querySelector(".text");
+$.fn.moveIt = function(){
+  var $window = $(window);
+  var instances = [];
+  
+  $(this).each(function(){
+    instances.push(new moveItItem($(this)));
+  });
+  
+  window.onscroll = function(){
+    var scrollTop = $window.scrollTop();
+    instances.forEach(function(inst){
+      inst.update(scrollTop);
+    });
+  }
+}
 
-    // Update positions
-    images.style.transform = `translateY(calc(-50% + ${scrollPosition / 2}px))`;
-    text.style.transform = `translateY(calc(-50% - ${scrollPosition / 2}px))`;
+var moveItItem = function(el){
+  this.el = $(el);
+  this.speed = parseInt(this.el.attr('data-scroll-speed'));
+};
+
+moveItItem.prototype.update = function(scrollTop){
+  this.el.css('transform', 'translateY(' + -(scrollTop / this.speed) + 'px)');
+};
+
+// Initialization
+$(function(){
+  $('[data-scroll-speed]').moveIt();
 });
