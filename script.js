1246bd253fe0169c39022f02705093949d5524c2
mxqt <maxquinntapara@gmail.com>
2024-12-11 11:33:01 +1300

    clean up code

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-$.fn.moveIt = function(){
+$.fn.moveIt = function () {
   var $window = $(window);
   var instances = [];
-  
-  $(this).each(function(){
+
+  $(this).each(function () {
     instances.push(new moveItItem($(this)));
   });
-  
-  window.onscroll = function(){
+
+  window.onscroll = function () {
     var scrollTop = $window.scrollTop();
-    instances.forEach(function(inst){
+    instances.forEach(function (inst) {
       inst.update(scrollTop);
     });
   }
 }
 
-var moveItItem = function(el){
+var moveItItem = function (el) {
   this.el = $(el);
   this.speed = parseInt(this.el.attr('data-scroll-speed'));
 };
 
-moveItItem.prototype.update = function(scrollTop){
+moveItItem.prototype.update = function (scrollTop) {
   this.el.css('transform', 'translateY(' + -(scrollTop / this.speed) + 'px)');
 };
 
 // Initialization
-$(function(){
+$(function () {
   $('[data-scroll-speed]').moveIt();
 });
